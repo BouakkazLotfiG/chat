745336aa289a42c931e4d77a7018abc02e15554f
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-07-06 19:19:45 +0100

    added input and message is being added to firestore

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { auth } from '../firebase';
 
 const ChatMessage = ({ message }) => {
   const { text, uid, photoURL } = message;
-  return <div>{text}</div>;
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} alt='avatar' />
+      <p>{text}</p>
+    </div>
+  );
 };
 
 export default ChatMessage;
--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,23 +1,49 @@
-import { collection } from 'firebase/firestore';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { useState } from 'react';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { db } from '../firebase';
+import { auth } from '../firebase';
 import ChatMessage from './ChatMessage';
 
 const ChatRoom = () => {
+  const [formValue, setFormValue] = useState('');
   const [messages] = useCollectionData(collection(db, 'message'), {
     idField: 'id',
   });
   console.log('messages:', messages);
+
+  const submitMessage = async (e) => {
+    e.preventDefault();
+    const { uid, photoURL } = auth.currentUser;
+    await addDoc(collection(db, 'message'), {
+      text: formValue,
+      createdAt: serverTimestamp(),
+      uid,
+      photoURL,
+    });
+    setFormValue('');
+  };
+
   return (
     <>
       <div>
-        {messages === true && messages.length === 0 ? (
-          messages.map((message) => (
-            <ChatMessage key={message.id} message={message} />
-          ))
-        ) : (
-          <h1>no messages yet</h1>
-        )}
+        <div>
+          {messages === true && messages.length === 0 ? (
+            messages.map((message) => (
+              <ChatMessage key={message.id} message={message} />
+            ))
+          ) : (
+            <h1>no messages yet</h1>
+          )}
+        </div>
+
+        <form onSubmit={submitMessage}>
+          <input
+            value={formValue}
+            onChange={(e) => setFormValue(e.target.value)}
+          />
+          <button type='submit'>send</button>
+        </form>
       </div>
     </>
   );
