9f9c2de8be8445df0d5c2b694aa5eb4fe5cec606
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-07-06 18:27:09 +0100

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
 
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyArRQcqklUoPhXUR3NWMNWfgn4dIyCYQOQ',
+  authDomain: 'chat-cc87d.firebaseapp.com',
+  projectId: 'chat-cc87d',
+  storageBucket: 'chat-cc87d.appspot.com',
+  messagingSenderId: '117948987889',
+  appId: '1:117948987889:web:4208489f9e94800ed92c50',
+  measurementId: 'G-Q9LVCQXX1W',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore(app);
+
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <header>
+        <h1>⚛️🔥💬</h1>
+      </header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
 }
